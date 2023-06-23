4f38b76b00d99272b04586195a7b502baf3a96f0
Trengland <englandvivial@gmail.com>
2023-06-22 22:41:37 -0400

    contact form field validations created

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,67 +1,126 @@
-import React from 'react'
-import {useState} from 'react' 
-import './style.css'
+import React, { useState } from 'react';
+import './style.css';
 
 function Contact() {
-  const [formState, setFormState] = useState({name: '', email: '', message: ''});
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [messageError, setMessageError] = useState('');
 
   function handleSubmit(e) {
-    e.preventDefault(); //makes sure form does not refresh page
-    console.log(formState);
+    e.preventDefault(); // prevents form from refreshing the page
+
+    // Check if name field is empty
+    if (!formState.name) {
+      setNameError('Name is required');
+    } else {
+      setNameError(''); // Clear the error if name field is not empty
+    }
+
+    // Check if email field is empty
+    if (!formState.email) {
+      setEmailError('Email is required');
+    } else {
+      setEmailError(''); // Clear the error if email field is not empty
+    }
+
+    // Check if message field is empty
+    if (!formState.message) {
+      setMessageError('Message is required');
+    } else {
+      setMessageError(''); // Clear the error if message field is not empty
+    }
+
+    // Submit the form only if all fields are non-empty
+    if (formState.name && formState.email && formState.message) {
+      console.log(formState);
+    }
   }
 
-  return (
-  <div>
-  <section className="contact-info">
-    <h2>Contact Info</h2>
+  const handleFieldBlur = (e) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value.trim();
 
-  <div id="address"> 
-    <address>
-        Give me a call to discuss a project or opportunity: <a href="tel:+19375806559">
-          (937) 580-6559
-        </a>
-<br></br>
-        Send me an email, I will get back with you ASAP: <a href="mailto:tiffanyrengland@gmail.com">
-          Tiffanyrengland@gmail.com </a>
-<br></br>
-          Check out my LinkedIn Profile to view my work history, references and skills: <a href="https://www.linkedin.com/in/tiffany-england-39374b55/" id="linkedin">
+    if (!fieldValue) {
+      alert(`${fieldName} is required`);
+    }
+  };
+
+  return (
+    <div>
+      <section className="contact-info">
+        <h2>Contact Info</h2>
+
+        <div id="address">
+          <address>
+            Give me a call to discuss a project or opportunity:{' '}
+            <a href="tel:+19375806559">(937) 580-6559</a>
+            <br />
+            Send me an email, I will get back with you ASAP:{' '}
+            <a href="mailto:tiffanyrengland@gmail.com">Tiffanyrengland@gmail.com</a>
+            <br />
+            Check out my LinkedIn Profile to view my work history, references, and skills:{' '}
+            <a href="https://www.linkedin.com/in/tiffany-england-39374b55/" id="linkedin">
               LinkedIn
-          </a>
-<br></br>
-          Visit my GitHub profile to see my past, present and future projects: <a href="https://github.com/Trengland" id="github">
-            GitHub
-          </a>
-    </address>
-  </div>
-  </section>
-<br></br>
-<br></br>
-  <form id="form" onSubmit={(e) => {
-        handleSubmit(e);
-        }}>
-<br></br>
-<br></br>
+            </a>
+            <br />
+            Visit my GitHub profile to see my past, present, and future projects:{' '}
+            <a href="https://github.com/Trengland" id="github">
+              GitHub
+            </a>
+          </address>
+        </div>
+      </section>
+
+      <form
+        id="form"
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
         <h3>Send me a message! I'll get back with you ASAP.</h3>
         <label> Name </label>
-        <input onChange={(e) => {
-          setFormState({...formState, name: e.target.value})
-        }} type="text" id="name" name="name" placeholder="Your Name" />
+        <input
+          onChange={(e) => {
+            setFormState({ ...formState, name: e.target.value });
+          }}
+          onBlur={handleFieldBlur}
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Your Name"
+        />
+        {nameError && <p className="error">{nameError}</p>}
 
         <label> Email </label>
-        <input onChange={(e) => {
-          setFormState({...formState, email: e.target.value})
-        }} type="email" id="email" name="email" placeholder="Your Email" />
+        <input
+          onChange={(e) => {
+            setFormState({ ...formState, email: e.target.value });
+          }}
+          onBlur={handleFieldBlur}
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Your Email"
+        />
+        {emailError && <p className="error">{emailError}</p>}
 
         <label> Message </label>
-        <textarea onChange={(e) => {
-          setFormState({...formState, message: e.target.value})
-        }} id="message" name="message" placeholder="Your Message"></textarea>
+        <textarea
+          onChange={(e) => {
+            setFormState({ ...formState, message: e.target.value });
+          }}
+          onBlur={handleFieldBlur}
+          id="message"
+          name="message"
+          placeholder="Your Message"
+        />
+        {messageError && <p className="error">{messageError}</p>}
 
         <button type="submit">Submit</button>
-     </form>
-     <br></br>
-     <br></br>
-  </div>
-  )}
+      </form>
+    </div>
+  );
+}
 
-export default Contact
+export default Contact;
