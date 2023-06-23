20e11ff11d30990d1ebe8bbe62b8cc08ca913526
Trengland <englandvivial@gmail.com>
2023-06-22 22:45:28 -0400

    fixing bug trying to deploy to github pages

--- src/App.js
+++ src/App.js
@@ -4,44 +4,25 @@ import Contact from './components/contact/contact';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
 import Projects from './components/projects/projects';
-// import {useState} from 'react';
 import Resume from './components/resume/resume';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
 function App() {
-// const [currentPage, setCurrentPage] = useState('/');
-return (
-  <> 
-
-<BrowserRouter baseName={"/"} >
-<Header/>
+  return (
+    <>
+      <BrowserRouter basename="/react-portfolio">
+        <Header />
         <Routes>
-          <Route 
-            path="/" 
-            element={<About />}
-          />
-          <Route 
-            path="/projects" 
-            element={<Projects />}
-          />
-          <Route 
-            path="/contact" 
-            element={<Contact />}
-          />
-          <Route
-            path="/resume"
-            element={<Resume />}
-          />
-          <Route 
-            path="*"
-            element={<About />}
-          />
+          <Route path="/" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<About />} />
         </Routes>
-    <Footer/>
-    </BrowserRouter>
-  </>
-)};
-
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
 
 export default App;
